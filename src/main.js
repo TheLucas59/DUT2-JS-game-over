4599e2aec8be190643d269274bdc1ac582aa6336
Eliott <eliott.collin.etu@univ-lille.fr>
2021-03-04 11:17:36 +0100

    Ajout page favoris

--- src/main.js
+++ src/main.js
@@ -4,16 +4,18 @@ import PizzaForm from './pages/PizzaForm';
 import Component from './components/Component';
 
 const pizzaList = new PizzaList([]),
-	aboutPage = new Component('p', null, 'ce site est génial'),
-	pizzaForm = new PizzaForm();
+	pizzaForm = new PizzaForm(),
+	favorisList = new Component('p', null, `Vous n'avez pas de favoris`),
+	aboutPage = new Component('p', null, 'ce site est génial');
 
 Router.titleElement = document.querySelector('.pageTitle');
 Router.contentElement = document.querySelector('.pageContent');
 Router.menuElement = document.querySelector('.mainMenu');
 Router.routes = [
 	{ path: '/', page: pizzaList, title: 'La carte' },
-	{ path: '/a-propos', page: aboutPage, title: 'À propos' },
 	{ path: '/ajouter-pizza', page: pizzaForm, title: 'Ajouter une pizza' },
+	{ path: '/favoris', page: favorisList, title: 'Mes Favoris' },
+	{ path: '/a-propos', page: aboutPage, title: 'À propos' },
 ];
 
 document.querySelector(
