400c918b2c600168bb5697b0fcb48d497bac75fc
Loïc DEMAY <loic.demay.etu@univ-lille.fr>
2021-03-22 14:01:10 +0100

    ajouter / enlever selon les fav actuels

--- src/components/GameThumbnail.js
+++ src/components/GameThumbnail.js
@@ -3,13 +3,18 @@ import Img from './Img.js';
 
 export default class GameThumbnail extends Component {
 	constructor(game) {
+		let favs = JSON.parse(localStorage.getItem('favoris'));
+		let favButton = "Ajouter aux favoris";
+		if(favs.includes(game.slug)){
+			favButton = "Enlever des favoris";
+		}
 		super('article', { name: 'class', value: 'gameThumbnail' }, [
 			new Component('a', [{ name: 'href', value: `/detail-${game.slug}` }, { name: "class", value: "gameLink"}], [ 
 				new Img(game.background_image),
 				new Component('section', null, [
 					new Component('h4', null, game.name),
 					new Component('p', null, `Note metacritic : ${game.metacritic}`),
-					new Component('button', [{name:'class', value:'favButton'}, {name:'alt', value:`${game.slug}`}], 'addFav'),
+					new Component('button', [{name:'class', value:'favButton'}, {name:'alt', value:`${game.slug}`}], `${favButton}`),
 				]),
 			]),
 		]);
